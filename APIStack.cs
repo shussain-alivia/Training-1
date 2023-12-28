08b92c5f603f28adc2afb9b3f58965b00cacebe7
hussains8 <66360797+hussains8@users.noreply.github.com>
2023-12-29 01:19:28 +0500

    Update APIStack.cs

--- APIStack.cs
+++ APIStack.cs
@@ -1,38 +1,96 @@
 using Amazon.CDK;
+using Amazon.CDK.AWS.Events;
+using Amazon.CDK.AWS.Events.Targets;
 using Amazon.CDK.AWS.Lambda;
 using Amazon.CDK.AWS.S3;
-using Amazon.CDK.AWS.Lambda.EventSources;
+using Amazon.CDK.AWS.S3.Events;
+using Amazon.CDK.AWS.CloudWatch;
+using Amazon.CDK.AWS.CloudWatch.Logs;
 
-namespace S3EventLambdaStack
+namespace YourNamespace
 {
-    public class S3EventLambdaStack : Stack
+    public class S3LambdaEventBridgeStack : Stack
     {
-        public S3EventLambdaStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        public S3LambdaEventBridgeStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
         {
+            // Create Lambda function
+            var lambdaFunction = new Function(this, "MyLambdaFunction", new FunctionProps
+            {
+                Runtime = Runtime.PYTHON_3_8,
+                Handler = "index.lambda_handler",
+                Code = Code.FromAsset("./lambda"), // Path to your Python Lambda code
+                Timeout = Duration.Seconds(30) // Adjust timeout as needed
+            });
+
             // Create an S3 bucket
-            var bucket = new Bucket(this, "MyS3Bucket", new BucketProps
+            var s3Bucket = new Bucket(this, "MyS3Bucket", new BucketProps
             {
-                BucketName = "my-unique-bucket-name", // Replace with your unique bucket name
-                RemovalPolicy = RemovalPolicy.DESTROY // Just an example, adjust according to your needs
+                RemovalPolicy = RemovalPolicy.DESTROY // Adjust as needed
             });
 
-            // Create a Lambda function
-            var lambdaFn = new Function(this, "MyS3EventLambda", new FunctionProps
+            // Grant permissions to the Lambda function to access the S3 bucket
+            s3Bucket.GrantReadWrite(lambdaFunction);
+
+            // Configure S3 event notification to trigger the Lambda for PutObject events
+            s3Bucket.AddEventNotification(EventType.OBJECT_CREATED, new LambdaDestination(lambdaFunction));
+
+            // Create an EventBridge event bus
+            var eventBus = new EventBus(this, "MyEventBus");
+
+            // Create an EventBridge rule to match the S3 event and trigger the Lambda
+            var eventRule = new Rule(this, "S3LambdaEventRule", new RuleProps
             {
-                Runtime = Runtime.DOTNET_CORE_3_1, // Change to your preferred runtime
-                Handler = "MyAssembly::MyNamespace.MyClass::MyHandler", // Update with your handler method
-                Code = Code.FromAsset("path/to/your/lambda/code"), // Path to your lambda function code
-                Timeout = Duration.Seconds(30) // Set timeout as needed
+                EventPattern = new EventPattern
+                {
+                    Source = new[] { "aws.s3" },
+                    DetailType = new[] { "AWS API Call via CloudTrail" },
+                    Detail = new
+                    {
+                        eventSource = new[] { "s3.amazonaws.com" },
+                        eventName = new[] { "PutObject" }
+                    }
+                },
+                EventBus = eventBus
             });
 
-            // Grant permission for Lambda to access S3 bucket
-            bucket.GrantRead(lambdaFn);
+            // Add Lambda function as a target for the EventBridge rule
+            eventRule.AddTarget(new LambdaFunction(lambdaFunction));
 
-            // Add S3 trigger to Lambda function
-            lambdaFn.AddEventSource(new S3EventSource(bucket, new S3EventSourceProps
+            // Create a CloudWatch Log Group
+            var logGroup = new LogGroup(this, "MyLogGroup", new LogGroupProps
             {
-                Events = new[] { S3EventType.OBJECT_CREATED } // Trigger Lambda on object creation
-            }));
+                LogGroupName = "/your/log/group/name" // Replace with your desired log group name
+            });
+
+            // Create a CloudWatch Log Stream for the Lambda logs
+            var logStream = new LogStream(this, "MyLogStream", new LogStreamProps
+            {
+                LogGroup = logGroup
+            });
+
+            // Add Lambda function as a target for streaming logs to the CloudWatch Log Group
+            lambdaFunction.LogGroup.AddSubscriptionFilter("LambdaLogsSubscription", new SubscriptionFilterOptions
+            {
+                Destination = logStream
+            });
+
+            // Create an EventBridge Log Group
+            var eventBridgeLogGroup = new LogGroup(this, "MyEventBridgeLogGroup", new LogGroupProps
+            {
+                LogGroupName = "/your/eventbridge/log/group/name" // Replace with your EventBridge log group name
+            });
+
+            // Create an EventBridge Log Stream for Lambda logs
+            var eventBridgeLogStream = new LogStream(this, "MyEventBridgeLogStream", new LogStreamProps
+            {
+                LogGroup = eventBridgeLogGroup
+            });
+
+            // Connect the EventBridge Log Stream to the Lambda logs
+            logStream.AddSubscriptionFilter("EventBridgeLogsSubscription", new SubscriptionFilterOptions
+            {
+                Destination = eventBridgeLogStream
+            });
         }
     }
 
@@ -41,7 +99,7 @@ namespace S3EventLambdaStack
         static void Main(string[] args)
         {
             var app = new App();
-            new S3EventLambdaStack(app, "S3EventLambdaStack");
+            new S3LambdaEventBridgeStack(app, "S3LambdaEventBridgeStack");
             app.Synth();
         }
     }
