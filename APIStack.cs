1fe7027fdf083fffbda87b84c6bfcdb9d07e749e
hussains8 <66360797+hussains8@users.noreply.github.com>
2024-03-21 02:06:57 +0500

    Update APIStack.cs

--- APIStack.cs
+++ APIStack.cs
@@ -1,27 +1,31 @@
 private void AddCorsOptions(Amazon.CDK.AWS.APIGateway.IResource apiResource, string url)
 {
-    apiResource.AddMethod("OPTIONS", new MockIntegration(new IntegrationOptions
+    // Define integration response for OPTIONS method
+    var integrationResponse = new IntegrationResponse
     {
-        IntegrationResponses = new[]
+        StatusCode = "200",
+        ResponseParameters = new Dictionary<string, string>
         {
-            new IntegrationResponse
-            {
-                StatusCode = "200",
-                ResponseParameters = new Dictionary<string, string>
-                {
-                    { "method.response.header.Access-Control-Allow-Headers", "Content-Type, X-Amz-Date, Authorization, X-Api-Key, X-Amz-Security-Token, X-Anz-User-Agent" },
-                    { "method.response.header.Access-Control-Allow-Origin", url },
-                    { "method.response.header.Access-Control-Allow-Credentials", "false" },
-                    { "method.response.header.Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE" }
-                }
-            }
-        },
+            { "method.response.header.Access-Control-Allow-Headers", "Content-Type, X-Amz-Date, Authorization, X-Api-Key, X-Amz-Security-Token, X-Anz-User-Agent" },
+            { "method.response.header.Access-Control-Allow-Origin", url },
+            { "method.response.header.Access-Control-Allow-Credentials", "false" },
+            { "method.response.header.Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE" }
+        }
+    };
+
+    // Define integration options for OPTIONS method
+    var integrationOptions = new IntegrationOptions
+    {
+        IntegrationResponses = new[] { integrationResponse },
         PassthroughBehavior = PassthroughBehavior.NEVER,
         RequestTemplates = new Dictionary<string, string>
         {
             { "application/json", "{\"statusCode\": 200}" }
         }
-    }), new MethodOptions
+    };
+
+    // Add OPTIONS method to the API resource
+    apiResource.AddMethod("OPTIONS", new MockIntegration(integrationOptions), new MethodOptions
     {
         MethodResponses = new[]
         {
@@ -39,3 +43,6 @@ private void AddCorsOptions(Amazon.CDK.AWS.APIGateway.IResource apiResource, str
         }
     });
 }
+
+// Call AddCorsOptions method
+AddCorsOptions(api.Root, url);
