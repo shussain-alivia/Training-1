3cca22b6e26013ec7e780592cc4acaf5b8999e1b
hussains8 <66360797+hussains8@users.noreply.github.com>
2024-03-21 00:41:02 +0500

    Update APIStack.cs

--- APIStack.cs
+++ APIStack.cs
@@ -1,13 +1,33 @@
 private void AddCorsOptions(Amazon.CDK.AWS.APIGateway.IResource apiResource, string url)
 {
-    apiResource.AddMethod("OPTIONS", new MockIntegration(new IntegrationOptions()), new MethodOptions
+    apiResource.AddMethod("OPTIONS", new MockIntegration(new IntegrationOptions()), new IntegrationResponses[]
     {
-        MethodResponses = new[]
+        new IntegrationResponse()
         {
-            new MethodResponse
+            StatusCode = ((int)HttpStatusCode.OK).ToString(),
+            ResponseParameters = new Dictionary<string, string>()
+            {
+                { "method.response.header.Access-Control-Allow-Headers", "Content-Type, X-Amz-Date, Authorization, X-Api-Key, X-Amz-Security-Token, X-Anz-User-Agent" },
+                { "method.response.header.Access-Control-Allow-Origin", url },
+                { "method.response.header.Access-Control-Allow-Credentials", "false" },
+                { "method.response.header.Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE" }
+            },
+            PassthroughBehavior = PassthroughBehavior.NEVER,
+            RequestTemplates = new Dictionary<string, string>()
+            {
+                { "application/json", "{\"statusCode\": 200}" }
+            }
+        }
+    });
+
+    new MethodOptions()
+    {
+        MethodResponses = new MethodResponse[]
+        {
+            new MethodResponse()
             {
                 StatusCode = ((int)HttpStatusCode.OK).ToString(),
-                ResponseParameters = new Dictionary<string, bool>
+                ResponseParameters = new Dictionary<string, bool>()
                 {
                     { "method.response.header.Access-Control-Allow-Headers", true },
                     { "method.response.header.Access-Control-Allow-Methods", true },
@@ -15,25 +35,6 @@ private void AddCorsOptions(Amazon.CDK.AWS.APIGateway.IResource apiResource, str
                     { "method.response.header.Access-Control-Allow-Origin", true }
                 }
             }
-        },
-        IntegrationResponses = new[]
-        {
-            new IntegrationResponse
-            {
-                StatusCode = ((int)HttpStatusCode.OK).ToString(),
-                ResponseParameters = new Dictionary<string, string>
-                {
-                    { "method.response.header.Access-Control-Allow-Headers", "'Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token,X-Anz-User-Agent'" },
-                    { "method.response.header.Access-Control-Allow-Origin", url },
-                    { "method.response.header.Access-Control-Allow-Credentials", "'false'" },
-                    { "method.response.header.Access-Control-Allow-Methods", "'OPTIONS,GET,PUT,POST,DELETE'" }
-                },
-                PassthroughBehavior = PassthroughBehavior.NEVER,
-                RequestTemplates = new Dictionary<string, string>
-                {
-                    { "application/json", "{\"statusCode\": 200}" }
-                }
-            }
         }
-    });
+    };
 }
