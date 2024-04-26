7c4161a69d3824c01682c3d62d9b0b676a2afd84
hussains8 <66360797+hussains8@users.noreply.github.com>
2024-04-27 02:45:56 +0500

    Update APIStack.cs

--- APIStack.cs
+++ APIStack.cs
@@ -1,40 +1,13 @@
-private void AddCorsOptions(Amazon.CDK.AWS.APIGateway.IResource apiResource, string url)
-{
-    var integrationResponse = new IntegrationResponse
+var response = await Polly.Policy
+    .Handle<KeyNotFoundException>()
+    .RetryAsync(RETRY_TIMES)
+    .ExecuteAsync(async () =>
     {
-        StatusCode = "200",
-        ResponseParameters = new Dictionary<string, string>
-        {
-            { "method.response.header.Access-Control-Allow-Headers", "Content-Type, X-Amz-Date, Authorization, X-Api-Key, X-Amz-Security-Token, X-Anz-User-Agent" },
-            { "method.response.header.Access-Control-Allow-Origin", url },
-            { "method.response.header.Access-Control-Allow-Credentials", "false" },
-            { "method.response.header.Access-Control-Allow-Methods", "OPTIONS, GET, PUT, POST, DELETE" }
-        }
-    };
+        var httpResponse = await _httpClient.GetAsync(_httpClient.BaseAddress).ConfigureAwait(false);
+        var responseBody = await httpResponse.Content.ReadAsStringAsync().ConfigureAwait(false);
+        return (httpResponse, responseBody);
+    })
+    .ConfigureAwait(false);
 
-    apiResource.AddMethod("OPTIONS", new MockIntegration(new IntegrationOptions
-    {
-        IntegrationResponses = new[] { integrationResponse },
-        PassthroughBehavior = PassthroughBehavior.NEVER,
-        RequestTemplates = new Dictionary<string, string>
-        {
-            { "application/json", "{\"statusCode\": 200}" }
-        }
-    }), new MethodOptions
-    {
-        MethodResponses = new[]
-        {
-            new MethodResponse
-            {
-                StatusCode = "200",
-                ResponseParameters = new Dictionary<string, bool>
-                {
-                    { "method.response.header.Access-Control-Allow-Headers", true },
-                    { "method.response.header.Access-Control-Allow-Methods", true },
-                    { "method.response.header.Access-Control-Allow-Credentials", true },
-                    { "method.response.header.Access-Control-Allow-Origin", true }
-                }
-            }
-        }
-    });
-}
+var httpResponseMessage = response.Item1;
+var responseBody = response.Item2;
